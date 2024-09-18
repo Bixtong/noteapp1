eddb849613491fc35e5a9df84975762d8d28bb7a
Bixtong <125838419+Bixtong@users.noreply.github.com>
2024-09-18 20:28:53 +0800

    Update db_connectors.php

--- include/db_connectors.php
+++ include/db_connectors.php
@@ -1,12 +1,12 @@
 <?php
 function connectDB() {
-    $host = "localhost";
-    $dbname = "noteapp";
-    $username = "root";
-    $password = "";
+    $host = getenv('DB_HOST') ?: 'localhost';
+    $dbname = getenv('DB_DATABASE') ?: 'noteapp';
+    $username = getenv('DB_USERNAME') ?: 'root';
+    $password = getenv('DB_PASSWORD') ?: '';
 
     try {
-        $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+        $conn = new PDO("mysql:host=$host;dbname=$dbname;port=" . getenv('DB_PORT'), $username, $password);
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         return $conn;
     } catch (PDOException $e) {
