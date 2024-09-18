abe9a0cbcfef40be74cc76777728f90fda29513b
Bixtong <125838419+Bixtong@users.noreply.github.com>
2024-09-18 20:49:49 +0800

    Update db_connectors.php

--- include/db_connectors.php
+++ include/db_connectors.php
@@ -1,13 +1,15 @@
 <?php
 function connectDB() {
-    $host = getenv('DB_HOST') ?: 'localhost';
-    $dbname = getenv('DB_DATABASE') ?: 'default'; // Update to match your ENV
-    $username = getenv('DB_USERNAME') ?: 'mysql'; // Update as necessary
-    $password = getenv('DB_PASSWORD') ?: '';
-    $port = getenv('DB_PORT') ?: '3306'; // Default port
+    $config = [
+        'host' => getenv('DB_HOST') ?: 'localhost',
+        'dbname' => getenv('DB_DATABASE') ?: 'default', // Default value
+        'username' => getenv('DB_USERNAME') ?: 'mysql', // Default value
+        'password' => getenv('DB_PASSWORD') ?: '',
+        'dbport' => getenv('DB_PORT') ?: '3306', // Default to 3306
+    ];
 
     try {
-        $conn = new PDO("mysql:host=$host;dbname=$dbname;port=$port", $username, $password);
+        $conn = new PDO("mysql:host={$config['host']};dbname={$config['dbname']};port={$config['dbport']}", $config['username'], $config['password']);
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         return $conn;
     } catch (PDOException $e) {
